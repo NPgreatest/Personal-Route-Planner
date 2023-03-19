5b3be4187bead2890053065684194082521263fe
npgreatest <38424@sina.com>
2023-03-19 18:00:52 +0800

    add the feature of add multiple plan's sites

--- vue/src/views/Plan.vue
+++ vue/src/views/Plan.vue
@@ -3,14 +3,7 @@
   <div class="background">
     <img :src="imgSrc" width="100%" height="100%" alt="" />
   </div>
-
-
-
-
 <div class="elForm">
-
-
-
   <el-form ref="form" :model="form" label-width="220px" style="margin: auto">
     <el-row  class="tag-area" :gutter="20" style="margin: auto; display: flex;flex-wrap: wrap; padding-top: 20px; padding-bottom: 20px "    >
     <li :key="item.tagid" v-for="item in tags">
@@ -26,43 +19,22 @@
       <el-input placeholder="起点" v-model="input" style="width: 200px"></el-input>
     </el-form-item>
 
-
-    <el-form-item  >
-      <span slot="label"><span style="color: #00B5AD;font-size: 24px;"> 选择第一个景点： </span></span>
-      <el-select v-model="a1" @change="changeSite1" placeholder="选择感兴趣的景点" value-key="id"  :disabled="dis1">
-        <el-option v-for="item in array1" :label="item.sname" :value="item.sid" :key="item.sid" ></el-option>
-      </el-select>
-
-    </el-form-item>
-
-
-    <el-form-item  >
-      <span slot="label"><span style="color: #00B5AD;font-size: 24px;"> 选择第二个景点： </span></span>
-      <el-select v-model="a2" @change="changeSite2" placeholder="选择感兴趣的景点" value-key="id" :disabled="dis2">
-        <el-option v-for="item in array2" :label="item.sname" :value="item.sid" :key="item.sid" ></el-option>
-      </el-select>
-
-    </el-form-item>
-
-    <el-form-item  >
-      <span slot="label"><span style="color: #00B5AD;font-size: 24px;"> 选择第三个景点： </span></span>
-      <el-select v-model="a3" @change="changeSite3" placeholder="选择感兴趣的景点" value-key="id" :disabled="dis3">
-        <el-option v-for="item in array3" :label="item.sname" :value="item.sid" :key="item.sid" ></el-option>
-      </el-select>
-
-    </el-form-item>
-
-
+    <div v-for="(item, index) in selectedSites" :key="index">
+      <el-form-item>
+        <span slot="label"><span style="color: #00B5AD;font-size: 24px;"> 选择第{{ index + 1 }}个景点： </span></span>
+        <el-select v-model="item.id" @change="changeSite(index,item.id)" placeholder="选择感兴趣的景点" value-key="id">
+          <el-option v-for="site in availableSites[index]" :label="site.sname" :value="site.sid" :key="site.sid"></el-option>
+        </el-select>
+        <el-button v-if="index < 4" @click="addSite(index)">添加景点</el-button>
+        <el-button v-if="index > 0" @click="removeSite(index)">删除景点</el-button>
+      </el-form-item>
+    </div>
     <b-avatar  v-for="item in select"   class="comment-avatar" :src="item.pic" size="15rem" ></b-avatar>
     <el-form-item>
       <el-button type="primary" @click="repick">重置</el-button>
       <el-button type="success" @click="finish">完成</el-button>
     </el-form-item>
-
-
-
   </el-form>
-
 </div>
 
 </div>
@@ -87,6 +59,15 @@ export default {
       a1:"",
       a2:"",
       a3:"",
+      availableSites: [
+        [],
+          [],
+          [],
+          [],
+          []
+      ],
+      selectedSites: [{ id: null }],
+      maxSiteCount: 5,
       input:"",
       selectid:0,
       dis1:false,
@@ -136,7 +117,7 @@ export default {
       select:[
         {sid:1,
           pic:"http://localhost:8080/images/1.jpg"
-        },
+        }
       ],
       array2:[],
       array3:[],
@@ -146,6 +127,49 @@ export default {
     this.getFirstSites()
   },
   methods:{
+    addSite(index) {
+      this.selectedSites.splice(index + 1, 0, { id: null });
+    },
+    removeSite(index) {
+      this.selectedSites.splice(index, 1);
+    },
+    changeSite1:async function(value){
+      this.select=[]
+      this.select.push({sid:value,pic: this.sites[value-1].pic})
+      this.dis1=true
+      const {data: res} = await this.$axios.get("/user/getrecommand",{params: {sid: this.select[0].sid}});
+      if (res.status === 1) {
+        this.array2 = res.data.length > 0 ? res.data[0] : this.array2;
+        this.array2 =this.array2.filter(item =>item.sid!=this.select[0].sid)
+      }else{
+        this.$message.error("获取推荐失败，请登录")
+        return
+      }
+    },
+    changeSite:async function(index,value) {
+      this.select.push()
+      this.select[index]=({sid:value,pic: this.sites[value-1].pic})
+      console.log(index,value)
+      const {data: res} = await this.$axios.get("/user/getrecommand",{params: {sid: value}});
+      if (res.status === 1) {
+        this.availableSites[index+1] = res.data.length > 0 ? res.data[0] : this.availableSites[index+1];
+        //this.availableSites[index+1] =this.availableSites[index].filter(item =>item.sid!=this.select[index].sid)
+      }else{
+        this.$message.error("获取推荐失败，请登录")
+        return
+      }
+      // const selectedSiteIds = this.selectedSites.map(item => item.id);
+      // const duplicateIndex = selectedSiteIds.findIndex(
+      //     (id, i) => id === this.selectedSites[index].id && i !== index
+      // );
+      // if (duplicateIndex !== -1) {
+      //   this.selectedSites[duplicateIndex].id = null;
+      //   this.$message({
+      //     message: "不能选择重复的景点",
+      //     type: "warning"
+      //   });
+      // }
+    },
     selected(id){
       this.selectid=id;
       localStorage.setItem('tags',id);
@@ -170,26 +194,14 @@ export default {
         var s1=this.randomids()
         this.array1=[]
         for(var item of s1){
-          this.array1.push(this.sites[item])
+          this.availableSites[0].push(this.sites[item])
         }
       } else {
       this.$message.error("获取景点库失败")
       return
       }
     },
-    changeSite1:async function(value){
-      this.select=[]
-      this.select.push({sid:value,pic: this.sites[value-1].pic})
-      this.dis1=true
-      const {data: res} = await this.$axios.get("/user/getrecommand",{params: {sid: this.select[0].sid}});
-      if (res.status === 1) {
-        this.array2 = res.data.length > 0 ? res.data[0] : this.array2;
-        this.array2 =this.array2.filter(item =>item.sid!=this.select[0].sid)
-      }else{
-        this.$message.error("获取推荐失败，请登录")
-        return
-      }
-    },
+
     changeSite2:async function(value){
       this.dis2=true
       this.select.push({sid:value,pic: this.sites[value-1].pic})
@@ -249,8 +261,8 @@ export default {
   background-size: 100% 100%;
 }
 .elForm{
-  top: 20%;
-  left: 35%;
+  top: 10%;
+  left: 30%;
   margin: auto;
   display: flex;
   justify-content: center;
